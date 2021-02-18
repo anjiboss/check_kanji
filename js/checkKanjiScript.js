07969a4e5f959ad3bb2f1510e3c54b35fe2e7821
anjiboss <nhu.nhu.face@gmail.com>
2021-02-19 00:59:55 +0900

    fetch validate-from-to show-the-word random-answer

--- js/checkKanjiScript.js
+++ js/checkKanjiScript.js
@@ -1,9 +1,71 @@
-const checkBtn = $("#checkBtn")
+const checkBtn = $("#checkBtn");
+let testHistory = [];
 
-const fromNum = $("#fromNum")
-const toNum = $("#toNum")
-const level = document.getElementById("selecter")
+let from;
+let to;
+const fromNum = $("#fromNum");
+const toNum = $("#toNum");
 
-checkBtn.click(()=>{
-    console.log(level.options[level.options.selectedIndex].innerHTML)
-})
+let words;
+
+async function changeLevel(level) {
+  if (level !== "Chọn cấp độ Kanji") {
+    const res = await fetch(`json/${level}.json`);
+    const data = await res.json();
+    $("#input-field").css("display", "block");
+    words = data;
+  }
+}
+checkBtn.click(() => {
+  from = fromNum.val();
+  to = toNum.val();
+  //   validation
+  if (from === "" || to === "") {
+    errorOutput("Fill in all the fields please");
+  } else if (from <= 0 || to > words.length) {
+    errorOutput("Error: You must input from range of 1 to " + words.length);
+  } else if (from > to) {
+    tmpNum = from;
+    from = to;
+    to = tmpNum;
+    //expected Output: from -> to ; to -> from
+    startTest(Number(from), Number(to));
+  } else {
+    errorOutput();
+    startTest(Number(from), Number(to));
+  }
+});
+const errorOutput = (eMessage) => {
+  $("#error-message").html(eMessage);
+};
+const startTest = (from, to) => {
+  $("#checker-zone").css("display", "block");
+  outKanji(from, to);
+};
+
+const outKanji = (from, to) => {
+  curTestNum = randomInt(from, to + 1);
+  testHistory.push(curTestNum);
+  $("#kanji").html(words[curTestNum].kanji);
+  outAnswer(curTestNum);
+};
+
+const outAnswer = (curTestNum) => {
+  let ans = randomInt(1, 5); // out 1 - 4
+  $(`#answer-${ans}`).html(words[curTestNum].hanViet);
+  for (let i = 1; i <= 4; i++) {
+    if (i !== ans) {
+      $(`#answer-${i}`).html(words[randomInt(1, words.length)].hanViet);
+    }
+  }
+};
+
+$(".answers").click(function () {
+  console.log($(this));
+});
+
+function randomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
